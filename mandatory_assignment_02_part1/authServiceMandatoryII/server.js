7c08f6a9a9e60ca60e829d76872b84f0cdae2df7
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-04-23 21:52:12 +0200

    Mandatory 2 - part 1 - AUTHService integration

---
+++ mandatory_assignment_02_part1/authServiceMandatoryII/server.js
@@ -0,0 +1,31 @@
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+app.use(express.json());
+
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+
+app.listen(3000, console.log("Server is running on port", 3000));
